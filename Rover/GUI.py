df49068a74283103f6da8514aa827fcfa1e60366
Simon lane <Simon.Lane@soton.ac.uk>
2023-07-01 15:13:20 +0100

    New version of GUI

--- Rover/GUI.py
+++ Rover/GUI.py
@@ -21,23 +21,11 @@ import threading, queue
 from wasatch.WasatchBus    import WasatchBus
 from wasatch.WasatchDevice import WasatchDevice
 
-from threading import Thread, Lock
-
-import pygame
-from pygame.constants import JOYBUTTONDOWN
-from pygame.constants import JOYBUTTONUP
-from pygame.constants import JOYHATMOTION
-from pygame.constants import JOYAXISMOTION
-
+import Gamepad
 
 class Rover(QtWidgets.QMainWindow):
     def __init__(self):
         super(Rover, self).__init__()
-        arm_address   = "/dev/cu.usbmodem14340000"
-        wheel_address = "/dev/cu.usbmodem14330000"
-        fps = 60
-        self.armLock = Lock()
-        self.wheelLock = Lock()
         self.first_cam = True
         self.spectrometer_connected = False
         self.thread_to_GUI = queue.Queue()
@@ -54,10 +42,66 @@ class Rover(QtWidgets.QMainWindow):
         self.laser_power = 100
         self.bat_p = [0,0] #battery percentage
         self.bat_v = [16.6,16.6] #battery voltage
-        self.xbox_thread = Thread(
-            target=self._xbox_meh,
-            name="Xbox controller thread"
-        )
+        self.use_uCs = False
+        self.camRate = 50 #(ms between frame grabs)
+        
+#         varables for Rover controllers
+        self.mode = 0 		# 0: startup - all off; 1: driving mode; 2: arm mode; 3: raman mode;
+        self.user = 0 		# 0: participant; 1: moderator;
+        self.speed = 0		# float -1.0 to +1.0; forward/backwards speed
+        self.turn = 0		# float -1.0 to +1.0; turning speed
+        self.arm_speed = 0  # float -1.0 to +1.0; arm forwards/backwards progression
+        self.arm_turn = 0
+# connect to game controllers
+        
+        # Wait for a connection
+        if not Gamepad.available():
+            print('Please connect Xbox Adaptive Controller...')
+            while not Gamepad.available():
+                time.sleep(1.0)
+        self.X1C = Gamepad.XboxTWO(joystickNumber=0)
+        print('Adaptive Controller connected :)',self.X1C.joystickNumber)
+        
+        if not Gamepad.available():
+            print('Please connect Xbox Adaptive Controller...')
+            while not Gamepad.available():
+                time.sleep(1.0)
+        self.X2C = Gamepad.XboxONE(joystickNumber=1)
+        print('Handheld controller connected :)',self.X2C.joystickNumber)
+        
+        self.X1C.startBackgroundUpdates()
+        self.X2C.startBackgroundUpdates()
+#     define button handler functions
+        self.X1C.addButtonPressedHandler(0, lambda:self.buttonPress(1,0))
+        self.X1C.addButtonPressedHandler(1, lambda:self.buttonPress(1,1))
+        self.X1C.addButtonPressedHandler(2, lambda:self.buttonPress(1,2))
+        self.X1C.addButtonPressedHandler(3, lambda:self.buttonPress(1,3))
+        self.X1C.addButtonPressedHandler(4, lambda:self.buttonPress(1,4))
+        self.X1C.addButtonPressedHandler(5, lambda:self.buttonPress(1,5))
+        self.X1C.addButtonPressedHandler(6, lambda:self.buttonPress(1,6))
+        self.X1C.addButtonPressedHandler(7, lambda:self.buttonPress(1,7))
+        self.X1C.addButtonPressedHandler(8, lambda:self.buttonPress(1,8))
+        self.X1C.addButtonPressedHandler(9, lambda:self.buttonPress(1,9))
+        self.X1C.addButtonPressedHandler(10, lambda:self.buttonPress(1,10))
+        self.X2C.addButtonPressedHandler(0, lambda:self.buttonPress(2,0))
+        self.X2C.addButtonPressedHandler(1, lambda:self.buttonPress(2,1))
+        self.X2C.addButtonPressedHandler(2, lambda:self.buttonPress(2,2))
+        self.X2C.addButtonPressedHandler(3, lambda:self.buttonPress(2,3))
+        self.X2C.addButtonPressedHandler(4, lambda:self.buttonPress(2,4))
+        self.X2C.addButtonPressedHandler(5, lambda:self.buttonPress(2,5))
+        self.X2C.addButtonPressedHandler(6, lambda:self.buttonPress(2,6))
+        self.X2C.addButtonPressedHandler(7, lambda:self.buttonPress(2,7))
+        self.X2C.addButtonPressedHandler(8, lambda:self.buttonPress(2,8))
+        self.X2C.addButtonPressedHandler(9, lambda:self.buttonPress(2,9))
+        self.X2C.addButtonPressedHandler(10, lambda:self.buttonPress(2,10))
+#  define axis handlers
+
+        self.X1C.addAxisMovedHandler(4, self.axisMove_speed_C1)
+        self.X1C.addAxisMovedHandler(3, self.axisMove_turn_C1)
+        self.X2C.addAxisMovedHandler(4, self.axisMove_speed_C2)
+        self.X2C.addAxisMovedHandler(3, self.axisMove_turn_C2)
+
+                
 
 
 # connect to CAMERAS
@@ -90,67 +134,56 @@ class Rover(QtWidgets.QMainWindow):
         self.cam2.set(3,320)
         self.cam2.set(4,240)
         
-# Connect to TEENSY BOARDS        
-        ports = list(serial.tools.list_ports.comports())
-        str_ports = []
-        for p in ports:
-            str_ = "%s" %p
-            str_ports.append(str_.split()[0])
+# Connect to TEENSY BOARDS
+        if self.use_uCs:
+            ports = list(serial.tools.list_ports.comports())
+            str_ports = []
+            for p in ports:
+                str_ = "%s" %p
+                str_ports.append(str_.split()[0])
 
-        print(str_ports)
-        # Set addresses
-        for port in str_ports:
-            print('testing:', port)
-            p = serial.Serial(port=port, baudrate=115200, timeout=0.5)
-            time.sleep(0.2) #essential to have this delay!
-            p.write(str.encode("/hello;\n"))
-            time.sleep(0.2)
-            reply = p.readline().strip()
-            print('reply:',reply)
-            if reply == b'power and arm board':
-                print('match: power n arm')
-                arm_address = port
-            if reply == b'wheels board':
-                print('match: wheels')
-                wheel_address = port
-            p.close()
-        
-        
-        print('done testing for teensys')
-        # # connect to power/arm board
-        self.arm = serial.Serial(port=arm_address, baudrate=115200, timeout=0.5)
-        time.sleep(0.5) #essential to have this delay!
-        # No need to locks in init function because nothing will use the device until it is discovered.
-        self.arm.write(str.encode("/hello;\n"))
+#             print(str_ports)
+            # Set addresses
+            for port in str_ports:
+#                 print('testing:', port)
+                p = serial.Serial(port=port, baudrate=115200, timeout=0.5)
+                time.sleep(0.2) #essential to have this delay!
+                p.write(str.encode("/hello;\n"))
+                time.sleep(0.2)
+                reply = p.readline().strip()
+#                 print('reply:',reply)
+                if reply == b'power and arm board':
+#                     print('match: power n arm')
+                    arm_address = port
+                if reply == b'wheels board':
+#                     print('match: wheels')
+                    wheel_address = port
+                p.close()
+            
+# connect to power/arm board
+            self.arm = serial.Serial(port=arm_address, baudrate=115200, timeout=0.5)
+            time.sleep(0.5) #essential to have this delay!
+            self.arm.write(str.encode("/hello;\n"))
+            reply = self.arm.readline().strip()
+#             print('reply:',reply)
+            if reply == b'power and arm board': print('power board connection established')
+            else:
+                print('power board connection failed')
+                self.arm.close()
+# connect to wheels board
+            self.wheel = serial.Serial(port=wheel_address, baudrate=115200, timeout=0.5)
+            time.sleep(0.5) #essential to have this delay!
+            self.wheel.write(str.encode("/hello;\n"))
+            reply = self.wheel.readline().strip()
+#             print('reply:',reply)
+            if reply == b'wheels board': print('wheel board connection established')
+            else:
+                print('wheel board connection failed')
+                self.wheel.close()
 
-        reply = self.arm.readline().strip()
-        print('reply:',reply)
-        if reply == b'power and arm board':
-            print('power board connection established')
-        else:
-            print('no power board connection')
-            self.arm.close()
-        
-        
-        # connect to wheels board
-        self.wheel = serial.Serial(port=wheel_address, baudrate=115200, timeout=0.5)
-        time.sleep(0.5) #essential to have this delay!
-        self.wheel.write(str.encode("/hello;\n"))
-
-        reply = self.wheel.readline().strip()
-        print('reply:',reply)
-        if reply == b'wheels board':
-            print('wheel board connection established')
-        else:
-            print('no wheels board connection')
-            self.wheel.close()
-        
-
-        
         # build GUI
         self.initUI()
-
-        # connect to spectrometer
+# connect to spectrometer
 #         try:
 #             self.instalise_spectrometer()
 #             self.spectrometer_connected = True
@@ -158,17 +191,18 @@ class Rover(QtWidgets.QMainWindow):
 #             print("Did not connect with spectrometer")
 #             print(e)
   
- # TIMERS  
+# TIMERS  
 # cameras
         self.getCamFeed = QtCore.QTimer()
         self.getCamFeed.setSingleShot(False)
         self.getCamFeed.timeout.connect(lambda: self.display_cams())
-        self.getCamFeed.start(50) 
+        self.getCamFeed.start(self.camRate) 
 # power/arm
-        self.getPowerReport = QtCore.QTimer()
-        self.getPowerReport.setSingleShot(False)
-        self.getPowerReport.timeout.connect(lambda: self.update_power_arm())
-        self.getPowerReport.start(5000) 
+        if self.use_uCs:
+            self.getPowerReport = QtCore.QTimer()
+            self.getPowerReport.setSingleShot(False)
+            self.getPowerReport.timeout.connect(lambda: self.update_power_arm())
+            self.getPowerReport.start(5000) 
 # dummy spectra timer       
 #         self.specTimer = QtCore.QTimer()
 #         self.specTimer.setSingleShot(False)
@@ -179,38 +213,49 @@ class Rover(QtWidgets.QMainWindow):
         self.task_checker.setSingleShot(False)
         self.task_checker.timeout.connect(lambda: self.check_tasks())
         self.task_checker.start(100)
-
-
-        # Now start the xbox controller polling thread
-        self.xbox_thread.start()
-    
-    def _xbox_meh(self):
-        pygame.init()
-        
-        # Count how many controllers connected
-        jy = pygame.joystick.Joystick
-        self._playerno = pygame.joystick.get_count()
-        print ("No. of controllers =", self._playerno)
-        if self._playerno == 0:
-            raise RuntimeError("Not enought XBOX controllers")
-
-        self.player_list = []
-
-        # Initialise controllers and get feedback
-        for i in range (self._playerno):
-            self.player_list.append(jy(i))
-            # Make sure each controller gets initialised
-            self.player_list[-1].init()
-            # Print out the name of the controllers
-            print (f"Controller {jy(i).get_instance_id()} =", jy(i).get_name())
-            # Rumble for 500 ms to confirm initialisation
-            self.player_list[-1].rumble(0.5,0.9,500)
-            time.sleep(0.01)
-        print("*"*80)
-        while True:
-            pass
+# game controller check    
+        self.game_checker = QtCore.QTimer()
+        self.game_checker.setSingleShot(False)
+        self.game_checker.timeout.connect(lambda: self.check_controllers())
+        self.game_checker.start(100)
 
     
+    def buttonPress(self,controller, button):
+        print('Button', button, 'presed by controller', controller)
+        if button == 3: # user change
+            if self.user == 1: self.user = 2
+            else: self.user = 1
+            
+        if controller == self.user:
+            if button == 2: self.mode = 1
+            if button == 0: self.mode = 2
+            if button == 1: self.mode = 3
+    
+    def axisMove_speed_C1(self, position):  #controller 1 changing speed
+        if(self.mode == 1 and self.user == 1): self.speed = int(position * 1000)
+        if(self.mode == 2 and self.user == 1): self.arm_speed 	= int(position * 1000)
+    
+    def axisMove_speed_C2(self, position):  #controller 1 changing speed
+        if(self.mode == 1 and self.user == 2): self.speed = int(position * 1000)
+        if(self.mode == 2 and self.user == 2): self.arm_speed = int(position * 1000)
+    
+    def axisMove_turn_C1(self, position):  #controller 1 changing speed
+        if(self.mode == 1 and self.user == 1): self.turn = int(position * 1000)
+        if(self.mode == 2 and self.user == 1): self.arm_turn = int(position * 1000)
+    
+    def axisMove_turn_C2(self, position):  #controller 1 changing speed
+        if(self.mode == 1 and self.user == 2): self.turn = int(position * 1000)
+        if(self.mode == 2 and self.user == 2): self.arm_turn = int(position * 1000)
+    
+    def check_controllers(self):
+        if self.mode == 1:
+            self.arm_board.write(str.encode("/speed.%s.%s;" %(self.speed,self.turn)))
+        if self.mode == 2:
+            print(self.arm_speed)
+            print(self.arm_turn)
+
+
+
     def check_tasks(self):
         if self.thread_to_GUI.qsize()>0:
             print("spectra picked up from queue")
@@ -280,19 +325,14 @@ class Rover(QtWidgets.QMainWindow):
         self.wavenumbers = device.settings.wavenumbers    
     
     def normalise(self, _in):
-
         scaled = np.interp(_in,(_in.min(),_in.max()),(0,65000))
         kernel = [0.2, 0.2, 0.2, 0.2, 0.2]
         smoothed = np.convolve(scaled, kernel, mode='same')
         return smoothed
-   
-    def serial_arm_command_with_response(self, command_str):
-        with self.armLock:
-            self.arm.write(str.encode(command_str))
-            return self.arm.readline().strip()
-
+    
     def update_power_arm(self):
-        reply = self.serial_arm_command_with_response("/report.power;")
+        self.arm.write(str.encode("/report.power;"))
+        reply = self.arm.readline().strip()
         power_data = reply.decode("utf-8").split("\t")
         self.bat_v = [float(power_data[1]),float(power_data[3])]
         self.bat_p[0] = max(    ((self.bat_v[0]-16.6)/4.2)*100.0,0) # express as percentage
@@ -423,22 +463,22 @@ class Rover(QtWidgets.QMainWindow):
         self.signal.setGeometry(QtCore.QRect(screenWidth-50,0,50,50))
         self.signal.setStyleSheet("background-color: #FF0000")
         
-        
-
-
 
     def closeEvent(self, event): #to do upon GUI being closed
-        self.getCamFeed.stop()
-        self.getPowerReport.stop()
 #         self.specTimer.stop()
         self.task_checker.stop()
+        if self.use_uCs:
+            self.getPowerReport.stop()
+            self.arm.close()
+            self.wheel.close()
         
-
-        self.arm.close()
-        self.wheel.close()
+        self.getCamFeed.stop()
         self.cam1.release()
         self.cam2.release()
 #         self.dev.disconnect()
+        self.game_checker.stop()
+        self.X2C.disconnect()
+        self.X1C.disconnect()
 
 if __name__ == '__main__':
     app = 0
@@ -446,3 +486,4 @@ if __name__ == '__main__':
     gui = Rover()
     gui.show()
     app.exec_()
+
